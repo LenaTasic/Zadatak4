ad62cabbd006e4b58bd34c8d3798bad5371f333a
LenaTasic <97707992+LenaTasic@users.noreply.github.com>
2023-12-11 10:49:49 -0500

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,16 @@
+public abstract class Product {
+    String name;
+    String barCode;
+    double basePrice;
+    public static final double PDV = 20;
+
+    public Product(String name, String barCode, double basePrice) {
+        this.name = name;
+        this.barCode = barCode;
+        this.basePrice = basePrice;
+    }
+
+    public abstract double calculatePriceWithTax();
+
+
+}
